7f410ea111ab35c1a9ff92519955fe3846fdfb6b
Dustin085 <dustin830927@gmail.com>
2024-10-08 19:11:33 +0800

    basic-finish

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,103 @@
+import { useState, useEffect, useRef } from "react";
 import ProductCard from "./components/ProductCard";
 
 function Products() {
+    const [productData, setProductData] = useState([]);
+    useEffect(() => {
+        let demoProductData = [
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+            {
+                id: "#TG-SC504",
+                name: "向日葵II<br />刺し子 ひまわり２",
+                category: "生地",
+                size: "32x37cm",
+                jpyPrice: 400,
+                ntdPrice: 250
+            },
+        ];
+        setProductData(demoProductData);
+    }, []);
+
+    const productCartBoradRef = useRef(null);
+    const productListRef = useRef(null);
+    const tabMainRef = useRef(null);
+    // tab頁籤
+    useEffect(() => {
+        let tabsArr = Array.from(tabMainRef.current.querySelectorAll(".tab-item"));
+        tabsArr.forEach((item, index) => {
+            item.addEventListener("click", () => {
+                switchActive(tabsArr, index);
+            });
+        })
+        // 切換active元素
+        function switchActive(arrToSwitch, targetIndex) {
+            arrToSwitch.forEach((arrEle) => {
+                arrEle.classList.remove('active');
+            });
+            arrToSwitch[targetIndex].classList.add('active');
+            // console.log("switched");
+        }
+    }, []);
+
+    // 依照下方面板的高度產生padding
+    const calcPaddingBotForProductList = () => {
+        let productCartBoradHeight = getComputedStyle(productCartBoradRef.current).height.replace("px", "");
+        let gapBetweenListAndBorad = 16;
+        productListRef.current.style.paddingBottom = (Number(productCartBoradHeight) + gapBetweenListAndBorad) + "px";
+    };
+    useEffect(() => {
+        calcPaddingBotForProductList();
+        window.addEventListener("resize", () => {
+            calcPaddingBotForProductList();
+        });
+    }, []);
+
+    const [cartList, setCartList] = useState([]);
+
     return (
         <div className="products-wrap">
             <div className="products-topbar">
                 <button type="button" className="go-back-btn" aria-label="Go Back Button"></button>
-                <ul className="tab-main">
+                <ul className="tab-main" ref={tabMainRef}>
                     <li >
                         <button type="button" className="tab-item active">商品名</button>
                     </li>
@@ -17,28 +109,53 @@ function Products() {
                     </li>
                 </ul>
             </div>
-            <ul className="product-list">
-                <li>
-                    <ProductCard />
-                </li>
-                <li>
-                    <ProductCard />
-                </li>
-                <li>
-                    <ProductCard />
-                </li>
-                <li>
-                    <ProductCard />
-                </li>
-                <li>
-                    <ProductCard />
-                </li>
-                <li>
-                    <ProductCard />
-                </li>
+            <ul className="product-list" ref={productListRef}>
+                {
+                    productData.map((item, index) => {
+                        return <li key={index}><ProductCard data={item} setCartList={setCartList} /></li>
+                    })
+                }
             </ul>
-            <div className="shopping-cart-borad">
-
+            <div className="products-cart-borad" ref={productCartBoradRef}>
+                <div className="amount-box">
+                    <p className="title">商品数</p>
+                    <div className="end-box">
+                        <div className="cart-icon"></div>
+                        <p className="amount">
+                            <span className="num">
+                                {
+                                    cartList.reduce((total, item) => {
+                                        return total + (typeof item.amount === "number" && item.amount);
+                                    }, 0)
+                                }
+                            </span>
+                            <span className="unit">個</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="price-box">
+                    <div className="jpy-box">
+                        <p className="unit">JPY</p>
+                        <p className="price">
+                            {
+                                cartList.reduce((total, item) => {
+                                    return total + (typeof item.jpyPrice === "number" && item.jpyPrice * item.amount);
+                                }, 0)
+                            }
+                        </p>
+                    </div>
+                    <div className="ntd-box">
+                        <p className="unit">NTD</p>
+                        <p className="price">
+                            {
+                                cartList.reduce((total, item) => {
+                                    return total + (typeof item.ntdPrice === "number" && item.ntdPrice * item.amount);
+                                }, 0)
+                            }
+                        </p>
+                    </div>
+                </div>
+                <button type="button" className="go-cart-page-btn">檢視購物車<br />カートを見る</button>
             </div>
         </div>
     );
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,21 +2,24 @@ import { useEffect, useRef, useState } from "react";
 // 動畫產生高度變化的套件
 import AnimateHeight from "react-animate-height";
 
-function ProductCard({ isFoldProp = true }) {
+function ProductCard({ isFoldProp = true, isInCartProp = false, setCartList = null, data = {
+    id: "#TG-SC504",
+    name: "向日葵II<br />刺し子 ひまわり２",
+    category: "生地",
+    size: "32x37cm",
+    jpyPrice: 400,
+    ntdPrice: 250
+} }) {
+    const { id, name, category, size, jpyPrice, ntdPrice } = data;
 
     const [isFold, setIsFold] = useState(isFoldProp);
     const [innerMenuHeight, setInnerMenuHeight] = useState(isFoldProp ? 0 : "auto");
-    const productCardRef = useRef(null);
     useEffect(() => {
         if (isFold) {
-            productCardRef.current.classList.remove("unfold");
             setInnerMenuHeight(0);
-        }else{
-            productCardRef.current.classList.add("unfold");
+        } else {
             setInnerMenuHeight("auto");
         }
-        // isFold ? productCardRef.current.classList.remove("unfold") : productCardRef.current.classList.add("unfold");
-        // isFold ? setInnerMenuHeight(0) : setInnerMenuHeight("auto");
     }, [isFold]);
 
     const toggleFoldHandler = () => {
@@ -25,38 +28,79 @@ function ProductCard({ isFoldProp = true }) {
         });
     };
 
+    const [amount, setAmount] = useState(0);
+    const amountChangeHandler = (diff) => {
+        setAmount((prevState) => {
+            if (prevState + diff >= 0) {
+                return prevState + diff;
+            } else {
+                return prevState;
+            }
+        });
+    };
+
+    const [isInCart, setIsInCart] = useState(isInCartProp);
+
+    const amountInCart = useRef(0);
+    const changeCartListHandler = () => {
+        amountInCart.current = amount;
+        if (setCartList && amount > 0) {
+            let cartItem = {
+                id: id,
+                amount: amount,
+                jpyPrice: 400,
+                ntdPrice: 250
+            };
+            setCartList([cartItem]);
+            setIsInCart(true);
+        } else {
+            setCartList([]);
+            setIsInCart(false);
+        }
+    };
+
     return (
-        <div className="product-card" onClick={toggleFoldHandler} ref={productCardRef}>
-            <div className="card-body">
+        <div className={"product-card" + (isInCart ? " in-cart" : "") + (isFold ? "" : " unfold")}>
+            <div className="card-body" onClick={toggleFoldHandler}>
                 <picture className="pic blank-pic">
                     {/* <img src="" alt="" /> */}
                 </picture>
                 <div className="content">
                     <div className="start-box">
                         <div className="name-box">
-                            <p className="product-id">#TG-SC504</p>
-                            <p className="product-name">向日葵II<br />刺し子 ひまわり２</p>
+                            <p className="product-id">{id}</p>
+                            <p className="product-name">
+                                {
+                                    name.split("<br />").map((item, index, arr) => {
+                                        // 最後一個段落不加斷行
+                                        return index === (arr.length - 1) ? <span key={index}>{item}</span> : <span key={index}>{item}<br /></span>
+                                    })
+                                }
+                            </p>
                         </div>
-                        <div className="tag">生地</div>
+                        <div className="tag">{category}</div>
                     </div>
                     <div className="end-box">
-                        <p className="product-size">32x37cm</p>
+                        <p className="product-size">{size}</p>
                         <p className="product-price">
-                            <span className="jpy-price">JPY 400</span>
+                            <span className="jpy-price">JPY {jpyPrice}</span>
                             <span> / </span>
-                            <span className="ntd-price">NTD 250</span>
+                            <span className="ntd-price">NTD {ntdPrice}</span>
                         </p>
                     </div>
                 </div>
+                {
+                    isInCart && <p className="amount-indicator">{amountInCart.current}</p>
+                }
             </div>
             <AnimateHeight height={innerMenuHeight} duration={150}>
                 <div className="inner-menu">
                     <div className="amount-ctrl-box">
-                        <button type="button" className="minus-btn"></button>
-                        <p className="amount">0</p>
-                        <button type="button" className="plus-btn"></button>
+                        <button type="button" className="minus-btn" onClick={() => { amountChangeHandler(-1) }}></button>
+                        <p className="amount">{amount}</p>
+                        <button type="button" className="plus-btn" onClick={() => { amountChangeHandler(1) }}></button>
                     </div>
-                    <button type="button" className="add-to-cart-btn">
+                    <button type="button" className="add-to-cart-btn" onClick={changeCartListHandler}>
                         <div className="cart-icon"></div>
                         <p className="text">加入 / カートに入れる</p>
                     </button>
